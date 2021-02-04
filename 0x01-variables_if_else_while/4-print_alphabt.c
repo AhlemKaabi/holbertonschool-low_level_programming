53d01be845c0cdf9569690bd9e4a4b26a1247e69
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-04 21:49:51 +0100

    fix code 0.4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,10 @@ int main(void)
 {
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-    if ((ch != 'q')&&(ch != 'e'))
-        putchar(ch);
+{
+if ((ch != 'q') && (ch != 'e'))
+putchar(ch);
+}
+putchar('\n');
 return (0);
 }
