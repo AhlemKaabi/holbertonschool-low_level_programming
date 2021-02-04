ef34cf71e3b136a3a115fb5872b04c5e84cc94ac
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-04 22:30:49 +0100

    adding file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints 10 decimal numbers
+* Return: success
+*/
+
+int main(void)
+{
+int i, j;
+for (i = 48 ; i < 58 ; i++)
+{
+putchar(i);
+for (j = 0 ; j < 1; j++)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
