a97afdeeb777d86ef8c4587455f7ff78be9616bb
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-04 20:32:50 +0100

    adding file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints 10 decimal numbers
+* Return: success
+*/
+
+int main(void)
+{
+int i;
+for (i = 'z'; i >= 'a'; i--)
+putchar(i);
+putchar('\n');
+return (0);
+}
