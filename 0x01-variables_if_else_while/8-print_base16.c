b90805dcbff22d2245b7a6cbc9bd4c1fa01acddd
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-04 22:04:10 +0100

    adding file 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints the alphabets in lowercase and uppercase
+* Return: success
+*/
+
+int main(void)
+{
+char ch0, ch1;
+for (ch0 = '0'; ch0 <= '9'; ch0++)
+putchar(ch0);
+for (ch1 = 'a'; ch1 <= 'f'; ch1++)
+putchar(ch1);
+putchar('\n');
+return (0);
+}
