a90cf1d121db567146b799ee353758f3c5387052
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-04 20:35:10 +0100

    adding file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints the alphabet in lowercase
+* Return: success
+*/
+
+int main(void)
+{
+char ch;
+ st
+for (ch = 'a'; ch <= 'z'; ch++)
+    if (putchar(ch) != 'qe')
+    putchar(ch)
+return (0);
+}
