4b940cc21e4b13d6bf6e915404a2f920330b8283
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-04 20:07:22 +0100

    adding file0.6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints 10 decimal numbers
+* Return: success
+*/
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
