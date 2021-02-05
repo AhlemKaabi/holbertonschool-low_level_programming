a40550bc3481e601ecebc002bf84e27aa043076b
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-05 23:04:08 +0100

    adding file 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints the alphabets in lowercase and uppercase
+* Return: success
+*/
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(float));
+return (0);
+}
