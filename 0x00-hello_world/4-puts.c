83628bc138ba1645f2c4784f67624cd0729cfb17
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-05 14:51:07 +0100

    00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
-#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints the alphabets in lowercase and uppercase
+* Return: success
+*/
+
+int main(void)
+{
+char ch;
 puts("Programming is like building a multilingual puzzle")
+putchar('\n');
+return (0);
+}
