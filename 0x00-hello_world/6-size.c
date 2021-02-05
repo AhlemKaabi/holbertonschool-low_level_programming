850e67968be3abbdda57ea8e2a8da85e6fcfad48
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-05 23:10:19 +0100

    adding file 7 0.2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of a int: %lu byte(s)\n", sizeof(int));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 printf("Size of a float int: %lu byte(s)\n", sizeof(float));
