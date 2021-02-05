cbb7e4518e3610c7a68a411b1799f702e7384d13
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-05 22:54:31 +0100

    adding file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints the alphabets in lowercase and uppercase
+* Return: success
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
