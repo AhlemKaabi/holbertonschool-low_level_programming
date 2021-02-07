99b2c22e2bfcc327045e80facced61c54b8041b4
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-07 13:43:41 +0100

    affing file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* prints the alphabets in lowercase and uppercase
+* Return: success
+*/
+
+int main(void)
+{
+scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
