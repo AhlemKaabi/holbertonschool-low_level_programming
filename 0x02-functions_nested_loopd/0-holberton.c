ac8648d4796f646941652381d721194c0ba636f4
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-09 10:41:55 +0100

    fix filegit add 0-holberton.c

--- 0x02-functions_nested_loopd/0-holberton.c
+++ 0x02-functions_nested_loopd/0-holberton.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "holberton.h"
 /**
 * main - entry point
 * Return: - success
-* print - print a message
 */
 
 int main(void)
 {
-print();
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
