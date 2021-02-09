bbd1d05bc08a3e1aa16f66e11089767e1f632884
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-09 10:42:33 +0100

    head file fix 0.3

--- 0x02-functions_nested_loopd/holberton.h
+++ 0x02-functions_nested_loopd/holberton.h
@@ -1,19 +1,4 @@
-int _putchar();
-/**
- * print - writes to stdout
- *
- * Return
- */
-void print(void)
-{
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
-_putchar('t');
-_putchar('o');
-_putchar('n');
-_putchar('\n');
-}
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+#endif
