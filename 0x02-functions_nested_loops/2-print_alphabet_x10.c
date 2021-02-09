cd352787c7e64522ae28bc0cec2b3c5266d83c04
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-09 14:26:10 +0100

    fix code 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 /**
-* main - entry point
-* prints 10 decimal numbers
+* print_alphabet_x10 - 10 lines alphabet
 * Return: success
 */
 void print_alphabet_x10(void)
@@ -10,6 +9,5 @@ int i;
 for (i = 0; i <= 10; i++)
 {
 print_alphabet();
-_putchar('\n');
 }
 }
