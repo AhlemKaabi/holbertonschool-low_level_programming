3af6ed136a35ee1f98cd046d997ba87d1501eb2c
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-10 11:40:14 +0100

    fix code

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 /**
 * _islower - function that checks for lowrcase
 * Return: success or fail
+*@c: parameter
 */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
-return 1;
+return (1);
 else
-return 0;
+return (0);
 }
