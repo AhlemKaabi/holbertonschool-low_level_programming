b96eed9913a1fd3629fdd0c60cc745f15d91be35
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-10 14:01:32 +0100

    00

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,13 +5,12 @@
 * Return: success
 */
 int print_last_digit(int n)
-{	
-	int r;
-	r = n % 10 ;
+{	int r;
+	r = n % 10;
 	if (n < 0)
 	{
 	r = -r;
 	}
-	_putchar(r + '0') ;
+	_putchar(r + '0');
 	return (r);
 }
