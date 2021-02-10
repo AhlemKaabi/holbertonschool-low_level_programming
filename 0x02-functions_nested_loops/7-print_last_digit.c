2b97ba0f288fba75e954d385c98490b604958c55
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-10 13:59:15 +0100

    00

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,22 +1,17 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "holberton.h"
 /**
-* main - entry point
-* prints 10 decimal numbers
+* print_last_digit - prints last digit
+* @n: parameter
 * Return: success
 */
-int _abs(int)
-{
-int r;
-scanf("give an integer",&r);
-x = (r+r)/2;
-return (x);
-}
-int main(void)
-{
-int n;
-int _abs(int );
-return (0);
+int print_last_digit(int n)
+{	
+	int r;
+	r = n % 10 ;
+	if (n < 0)
+	{
+	r = -r;
+	}
+	_putchar(r + '0') ;
+	return (r);
 }
