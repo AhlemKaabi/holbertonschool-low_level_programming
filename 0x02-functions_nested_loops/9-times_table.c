fb2615cbd30b86a1b60199595087fdf88240526a
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-10 17:19:53 +0100

    adding files with code fixed

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+* times_table - function that prints the 9 times table, starting with 0.
+* Return: success
+*/
+void times_table(void)
+{
+	int i, j, x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			x = i * j;
+			if (x < 10)
+			{
+				if (j != 9)
+				{
+					_putchar((x % 10) + '0');
+					_putchar(44);
+					_putchar(32);
+				}
+				else
+				{
+					_putchar((x % 10) + '0');
+				}
+
+			}
+			else
+			{
+				if (j != 9)
+				{
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+					_putchar(44);
+					_putchar(32);
+				}
+				else
+				{
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -11,4 +11,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 int add(int x , int y);
 void print_to_98(int n);
+void times_table(void);
 #endif
