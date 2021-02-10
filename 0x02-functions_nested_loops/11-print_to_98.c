76ddef92c0c9dabaa5e4ff69ede37b74a054b30a
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-10 16:19:23 +0100

    00

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,26 @@ void print_to_98(int n)
 
 	if (n == 98)
 	{
-		printf("98");
+		printf("98\n");
 	}
 	else if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
+			if (i != 98)
 			printf("%d, ", i);
+			else
+			printf("%d\n", i);
 		}
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
 		{
+			if (i != 98)
 			printf("%d, ", i);
+			else
+			printf("%d\n", i);
 		}
 	}
 }
