cce13d1cf2c9d1efdee9e7542d36b06e3f628fd6
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-11 20:29:44 +0100

    adding file 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - entry piont
+* Return: success or fail
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	if ((i % 3) == 0)
+	{
+		printf("Fizz ");
+	}
+	else  if ((i % 5) == 0)
+	{
+		printf("Buzz ");
+	}
+	else if ((i % 3) == 0 && (i % 5) == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else
+	printf("%d ", i);
+return (0);
+}
