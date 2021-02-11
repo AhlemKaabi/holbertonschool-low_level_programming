ca5cdd42dfc7c3988b8ed06400991f0326cdb460
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-11 19:59:46 +0100

    fixing code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n > 0)
 	{
-		if (n > 0)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar(95);
 		}
-		else
-		{
 			_putchar('\n');
-		}
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
