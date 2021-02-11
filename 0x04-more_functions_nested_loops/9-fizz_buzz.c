d420c150abdd1fe517415d73264e9861bbf655d9
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-11 20:34:35 +0100

    adding file 90.3

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,11 +8,11 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	if ((i % 3) == 0)
+	if ((i % 3) == 0 && (i % 5) != 0)
 	{
 		printf("Fizz ");
 	}
-	else  if ((i % 5) == 0)
+	else  if ((i % 5) == 0 && (i % 3) != 0)
 	{
 		printf("Buzz ");
 	}
