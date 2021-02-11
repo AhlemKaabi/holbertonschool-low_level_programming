4e0323e908ba0e5c82749905beaae3c8b70a28df
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-11 22:43:51 +0100

    00

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,22 @@ int main(void)
 	{
 		if ((i % 3) == 0 && (i % 5) != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else  if ((i % 5) == 0 && (i % 3) != 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+	printf(" ");
 	}
 printf("\n");
-
 return (0);
 }
