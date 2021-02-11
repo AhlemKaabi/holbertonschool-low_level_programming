a3e9bd833bf4fd4e69d37d0b7c9f0985c7902e76
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-11 22:28:09 +0100

    00

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,23 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	if ((i % 3) == 0 && (i % 5) != 0)
 	{
-		printf("Fizz ");
-	}
-	else  if ((i % 5) == 0 && (i % 3) != 0)
-	{
-		printf("Buzz ");
-	}
-	else if ((i % 3) == 0 && (i % 5) == 0)
-	{
-		printf("FizzBuzz ");
-	}
-	else
-	{
-		printf("%d ", i);
+		if ((i % 3) == 0 && (i % 5) != 0)
+		{
+			printf("Fizz ");
+		}
+		else  if ((i % 5) == 0 && (i % 3) != 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
 	}
 printf(" \n");
 
