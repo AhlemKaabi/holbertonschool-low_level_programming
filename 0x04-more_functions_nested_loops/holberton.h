0edb127fe7d7bdc95e6975179720f7559febcf69
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-11 19:46:58 +0100

    adding .h file

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,4 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int print_numbers(void);
+int print_most_numbers(void);
+int more_numbers(void);
+void print_line(int n);
+void print_square(int size);
+void print_diagonal(int n);
 #endif
