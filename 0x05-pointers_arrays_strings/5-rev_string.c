cd31072fc0b88776da8044e001b181affa3b87a1
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-15 22:25:56 +0100

    fix code 0.2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,18 @@
 void rev_string(char *s)
 {
 	int i, j, x;
+	char tmp;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
 		i++;
 		x = i;
 	}
-	for (j = 0; j <= x; j++)
-		s[j] = s[ (x - 1) - j];
+	for (j = 0; j <= (x - 1) / 2; j++)
+	{
+		tmp = s[j];
+		s[j] = s[x - 1 - j];
+		s[x - 1 - j] = tmp;
+	}
 }
