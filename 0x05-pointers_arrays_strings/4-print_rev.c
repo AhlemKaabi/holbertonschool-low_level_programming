e6babd3e9c8de28526a595f212bbc8b50ff827f3
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-15 12:38:25 +0100

    adding task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_rev - function that prints a string in revers
+*@s: parameter pointer
+*/
+void print_rev(char *s)
+{
+	int i, j, x;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+		x = i;
+	}
+	for (j = x; j >= 0; j--)
+	{
+	_putchar(s[j]);
+	}
+	_putchar('\n');
+}
