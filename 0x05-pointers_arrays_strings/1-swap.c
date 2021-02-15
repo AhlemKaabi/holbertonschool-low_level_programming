fe9717f5385d015b180d11480f94cc34113e4a7a
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-15 10:40:40 +0100

    adding task #1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* swap_int - function that swap the values of two integers.
+*@a: pointer parameter
+*@b: pointer parameter
+*/
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
