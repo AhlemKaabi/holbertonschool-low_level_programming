26fdab1fbf092f3671082f1562c61d269ec22374
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-15 22:48:33 +0100

    task 6 v 0.1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* puts2 - function that prints every other character of a string.
+*@str: parameter pointer
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);	
+		i+=2;
+	}
+	_putchar('\n');
+}
