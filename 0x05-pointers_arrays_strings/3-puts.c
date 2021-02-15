342233dc0ea9b7cb15da656e64efc1c83b46660f
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-15 12:25:19 +0100

    adding task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _puts - function that prints a string
+*@str: parameter pointer
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
