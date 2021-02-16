de6c3c5608f205afbd5dc80d97b54903e3eeec37
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-16 11:07:00 +0100

    focus same i git add 9-strcpy.c  same loop

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _strcpy - function that copies a string pointed to by src.
+*@dest: pointed to string
+*@src: pointing by strinng
+* Return: the pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
