f9d6ab2aa88cb0edbed45543bbed3ef4da72db75
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-16 11:15:31 +0100

    when to use pointer and when to use the content

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,9 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (*(src+ i) != '\0')
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 		i++;
 	}
 	return (dest);
