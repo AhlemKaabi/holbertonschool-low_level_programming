6b7084d9d69ea8524b74beee8c5aa22ca1ad9cbc
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-18 15:11:37 +0100

    adding file 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+# include "holberton.h"
+#include <stdio.h>
+/*
+*_strncpy - that copies a string
+* Return: string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *p = dest;
+
+	int j;
+
+	j = 0;
+	while (*src != '\0' && j <= n - 1)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		j++;
+	}
+	while (j < n)
+	{
+		*dest = '\0';
+		j++;
+	}
+	return (p);
+}
