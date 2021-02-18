6b54f64a5dafe06bda85f2edc51f7e5404146cc3
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-18 23:29:07 +0100

    fix betty

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,12 +7,14 @@
 char *leet(char *ch)
 {
 	int i, j;
+
 	char s[] = "aA4eE3oO0tT7lL1";
+
 	for (i = 0 ; ch[i] != '\0'; i++)
 	{
-		for(j = 0; s[j] != '\0'; j = j+3)
+		for (j = 0; s[j] != '\0'; j = j + 3)
 		{
-			if (ch[i] == s[j] || ch[i] == s[j+1])
+			if (ch[i] == s[j] || ch[i] == s[j + 1])
 			{
 				ch[i] = s[j + 2];
 			}
