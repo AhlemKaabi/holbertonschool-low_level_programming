2e463c972e7c5bc02713307f86a8c0d767691a61
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-18 22:23:27 +0100

    adding file 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+# include "holberton.h"
+#include <stdio.h>
+/**
+* cap_string - function
+*@ch: para
+* Return: string
+*/
+char *cap_string(char *ch)
+{
+	int i;
+
+	for (i = 0 ; ch[i] != '\0'; i++)
+	{
+		if (*(ch + 0) >= 'a' && *(ch + 0) <= 'z')
+			*ch = *ch - 32;
+			/*printf(" ch is : %c\n" , *(ch + 0));*/
+		if (*(ch + i) == ' ' || *(ch + i) == '\n' || *(ch + i) == ','
+		|| *(ch + i) == ';' || *(ch + i) == '.' || *(ch + i) == '?' ||
+		*(ch + i) == '"' || *(ch + i) == '(' || *(ch + i) == ')' ||
+		*(ch + i) == '{' || *(ch + i) == '}' || *(ch + i) == '\t')
+		{
+			if (*(ch + i + 1) >= 'a' && *(ch + i + 1) <= 'z')
+			{
+			*(ch + i + 1) = *(ch + i + 1) - 32;
+
+			}
+		/*printf(" chpart2 is : %c\n" , *(ch + i));*/
+		}
+	}
+	return (ch);
+}
