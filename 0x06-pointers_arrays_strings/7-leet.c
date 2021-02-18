c3869796d2a0b9c4e2b8acc25892ef8e87b2fc63
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-18 23:27:04 +0100

    adding file 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+# include "holberton.h"
+/**
+* leet - function
+*@ch: para
+* Return: string
+*/
+char *leet(char *ch)
+{
+	int i, j;
+	char s[] = "aA4eE3oO0tT7lL1";
+	for (i = 0 ; ch[i] != '\0'; i++)
+	{
+		for(j = 0; s[j] != '\0'; j = j+3)
+		{
+			if (ch[i] == s[j] || ch[i] == s[j+1])
+			{
+				ch[i] = s[j + 2];
+			}
+		}
+	}
+	return (ch);
+}
