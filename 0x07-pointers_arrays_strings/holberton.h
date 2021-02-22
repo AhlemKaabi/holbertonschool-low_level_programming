a864ea928950b93a78ff0aeddd7074213b6c938c
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-22 15:15:49 +0100

    adding modified .h file

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,4 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 #endif
