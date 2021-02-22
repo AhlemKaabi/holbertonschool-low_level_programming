9cc6701c2b838c86ac9cb1c9438105e5b7613c78
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-22 15:09:20 +0100

    delete break

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int j, i;
 
-	for (i = 0; s[i] == '\0'; i++)
+	for (i = 0; *(s + i) == '\0'; i++)
 	{
 		for (j = 0; j < n; j++)
 		{
-			s[j] = b;
+			*(s + j) = b;
 		}
 	}
 	return (s);
