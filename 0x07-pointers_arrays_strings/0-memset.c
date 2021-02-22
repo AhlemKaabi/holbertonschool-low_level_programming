5bdbc9ccbf79307785fe8a1d0240d9009f8ae077
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-22 09:25:35 +0100

    adding task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* _memset - function that fills memory with a constant byte.
+*@b: character
+*@n: the number of bytes to be changed
+*@s: the string
+* Return: the modified string
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j, i;
+
+	for (i = 0; s[i] == '\0'; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			s[j] = b;
+		}
+		break;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+char *_memset(char *s, char b, unsigned int n);
+#endif
