907e5e76fc14501c4f3c57e20b34e50721f25824
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-22 15:36:06 +0100

    adding task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+* _strchr - that locates a character in a string.
+*@s: the string.
+*@c: the character.
+* Return: the modified string s.
+*/
+char *_strchr(char *s, char c)
+{
+	for (;; s++)
+	{
+			if (*s == c) return (s);
+	}
+	return (NULL);
+}
