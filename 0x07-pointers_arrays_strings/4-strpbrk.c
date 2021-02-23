f5bb8afd00303facb996b4a73ad464a1f7426470
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-23 14:22:44 +0100

    pfffffffffffff

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
-#include <stddef.h>
+
+#include <stdio.h>
 /**
 * _strpbrk - function that searches a string for any of a set of bytes.
 *@s: the first string.
@@ -8,19 +9,23 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
+	int test;
 	char *t;
 
+	test = 0;
 	t = s;
 	while (*s != '\0')
 	{
-		while (*accept++ != '\0')
+		while (*accept++ == '\0')
 		{
 			if (*s == *accept)
+			test = 1;
 			t = s;
-			break;
 		}
 		s++;
 	}
-
-	return (t);
+	if (test == 1)
+		return(t);
+	else
+	return (0);
 }
