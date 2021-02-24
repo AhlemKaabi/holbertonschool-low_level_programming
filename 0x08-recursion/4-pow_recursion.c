f24ff289b03a5409287d0aa10a34751f3c9a3b9a
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-24 23:28:03 +0100

    fix code

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,23 @@
 #include "holberton.h"
 /**
-*
+* _pow_recursion - function that returns
+* the value of x raised to the power of y
+*@x: the number.
+*@y: another one.
+*Return: factoriel of n..
 */
 int _pow_recursion(int x, int y)
 {
-	
-	if (y == 1)
+	if (y == 0)
 	{
-		return(1);
+		return (1);
 	}
 	else if (y < 0)
 	{
-		return(-1);
-	}	
+		return (-1);
+	}
+	else if (x == 0)
+		return (0);
 	else
-		return(x  * _pow_recursion(x , y - 1));
+		return (x  * _pow_recursion(x, y - 1));
 }
