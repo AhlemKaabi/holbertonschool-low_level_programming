1396112b576b0da3a1eb495d8fe6d59f1c94dc3b
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-26 17:44:11 +0100

    afdding task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* test_sqrt - function.
+*@i: our test
+*@x: the number
+*Return: the sqrt
+*/
+int test_sqrt(int x, int i)
+{
+	if (i == x && ((i * i) % 2 != 0))
+		return (-1);
+	if (i * i == x)
+		return (i);
+	return (test_sqrt(x, i + 1));
+}
+/**
+* _sqrt_recursion - function that returns the natural square
+* root of a number.
+*@n: the number
+*Return: is it a primenumber or not.
+*/
+int _sqrt_recursion(int n)
+{
+	if (n <= 2)
+		return (-1);
+	return (test_sqrt(n, 3));
+}
