c441e5e7e3a0d8065271ea547d79abd1052f15dc
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-26 17:44:50 +0100

    adding task6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* is_prime_number - function test prime number
+*@n: the number
+*Return: is it a primenumber or not.
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (test_prime(n, n / 2));
+}
+/**
+* test_prime - function.
+*@i: the number
+*@n: numb
+*Return: int
+*/
+int test_prime(int n, int i);
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (test_prime(n, i - 1));
+}
