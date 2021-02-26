8cc1fcbebb6e9bedbae5609bc1cf04d487561114
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-26 22:19:17 +0100

    rev code

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,11 @@
 #include "holberton.h"
 /**
-* is_prime_number - function test prime number
-*@n: the number
-*Return: is it a primenumber or not.
-*/
-int is_prime_number(int n)
-{
-	if (n <= 1)
-		return (0);
-	return (test_prime(n, n / 2));
-}
-/**
 * test_prime - function.
 *@i: the number
 *@n: numb
 *Return: int
 */
-int test_prime(int n, int i);
+int test_prime(int n, int i)
 {
 	if (i == 1)
 	{
@@ -28,3 +17,15 @@ int test_prime(int n, int i);
 	}
 	return (test_prime(n, i - 1));
 }
+/**
+* is_prime_number - function test prime number
+*@n: the number
+*Return: is it a primenumber or not.
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (test_prime(n, n / 2));
+}
+
