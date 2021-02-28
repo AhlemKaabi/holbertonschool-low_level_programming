26ef0428afbd29606d462f1ed211e8a06391b1b1
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-02-28 23:45:34 +0100

    XXXDDDDD

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,16 @@
 #include "holberton.h"
+/**
+* _puts - function
+* @s : string
+* Return: void
+*/
 void _puts(char *s)
 {
-
+	int i = 0;
+	while(*(s + i))
+	{
+		_putchar(*(s + i));
+		i = i +1;
+	}
+	_putchar('\n');
 }
