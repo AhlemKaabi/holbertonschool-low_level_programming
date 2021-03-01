463fca44154e19c4fa9e0b7fffaf59ffaf1b8d47
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-01 11:26:28 +0100

    ...

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: number of arguments
+* @argv: arrany of the argc arguments content
+* Return: integer
+*/
+int main(int argc, char *argv[])
+{
+	argv[argc]
+	printf("%i\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: number of arguments
+* @argv: arrany of the argc arguments content
+* Return: integer
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
