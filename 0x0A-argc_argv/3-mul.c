368b1d93fedea782986e98c47360dcd51829e3a1
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-01 12:04:33 +0100

    00000

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+* @argc: number of arguments
+* @argv: arrany of the argc arguments content
+* Return: integer
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
