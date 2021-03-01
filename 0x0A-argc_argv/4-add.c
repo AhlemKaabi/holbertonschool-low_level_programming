3c17e3b198218222059724016ad9f4ac5981a5e2
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-01 16:08:38 +0100

    != '\0' and fix betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,10 +18,10 @@ int main(int argc, char *argv[])
 		{
 			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				if (!(argv[i][j] >='0' && argv[i][j] <= '9'))
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 				{
 					printf("Error\n");
-					return (1);	
+					return (1);
 				}
 				else
 				{
