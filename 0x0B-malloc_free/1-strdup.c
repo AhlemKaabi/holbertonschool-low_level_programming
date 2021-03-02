4a9f7a2d7a57c943c01e10f99ef7dd460f9977f8
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-02 19:06:03 +0100

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+* _strdup - fuction that returns a pointer to a newly
+* allocated space in memory, which contains a copy of
+* a string given as parameter.
+* @str: the given string.
+* Return: a copy of the given string
+*/
+char *_strdup(char *str)
+{
+	
+	int length = 0;
+	char *str_copy;
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+	str_copy = (char*)malloc(length * sizeof(char));
+	if (str_copy == NULL)
+		return(NULL);
+	else
+	{
+		for (j = 1; j <= length; j++)
+		{
+			str_copy[j] = str[j];
+		}
+		return (str_copy);
+	}
+}
