40b871f27bbbf2a5771d2520e44e4d666d91b995
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-02 17:22:54 +0100

    starting the task!

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+* create_array - function that creates an array
+* of chars and intialized with a specific char.
+* @size: the size of the array
+* @c: the chars to initialize the array with
+* Return: the array!
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	if (size > 0)
+	{
+		ar = malloc(size * sizeof(char));
+		ar[0] = c;
+		for (i = 1; i <= size - 1; i++)
+		{
+			ar[i] = c;
+		}
+		return (ar);
+	}
+	else
+		return (NULL);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+char *create_array(unsigned int size, char c);
+#endif
