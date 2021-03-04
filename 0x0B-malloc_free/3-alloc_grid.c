6e486fd0b014a3104003f979ad931e0b74a25894
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-04 14:49:46 +0100

    free the alocated memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		ar[i] = malloc(width * sizeof(int));
 		if (ar[i] == NULL)
 		{
-			for (h = 0; h < width; h++)
+			for (h = 0; h < i; h++)
 			{
 				free(ar[i]);
 			}
