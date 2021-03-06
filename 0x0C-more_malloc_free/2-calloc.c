f1b7afd77d5219efe1aa991000a9fc0d67a096a6
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-06 23:25:18 +0100

    size

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		arr[i] = 0;
 	return (arr);
 }
