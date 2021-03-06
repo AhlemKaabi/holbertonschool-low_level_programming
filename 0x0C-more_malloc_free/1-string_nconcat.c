72fcfbce1606270f4132fd6b7d6554913d2bd5f2
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-06 17:39:00 +0100

    adding 01

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,61 +11,27 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int tot_length = 0, i, j, L1 = 0, L2 = 0, x, h;
+	unsigned int i, j, L1 = 0, L2 = 0, x, h;
 	char *s;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		L1++;
-	}
 	for (j = 0; s2[j] != '\0'; j++)
-	{
 		L2++;
-	}
 	if ( n >= L2)
-	{
-		tot_length = L1 + L2;	
-		s = malloc(tot_length + 1 * sizeof(char));
-		if (s == NULL)
-		{
-			return (NULL);
-		}
-		for (x = 0; s1[x] != '\0'; x++)
-		{
-			s[x] = s1[x];
-		}
-		for (h = 0; h <= L2 ; h++)
-		{
-			s[L1 + h] = s2[h];
-		}
-		s[L1 + h] = '\0';
-		
-	}
-	if (n < L2)
-	{
-		tot_length = L1 + n;
-		s = malloc(tot_length + 1 * sizeof(char));
-		if (s == NULL)
-		{
-			return (NULL);
-		}
-		for (x = 0; s1[x] != '\0'; x++)
-		{
-			s[x] = s1[x];
-		}
-		for (h = 0; h <= n ; h++)
-		{
-			s[L1 + h] = s2[h];
-		}
-		s[L1 + h] = '\0';
-	}
+		s = malloc(L1 + L2 + 1 * sizeof(char));
+	else
+		s = malloc(L1 + n + 1 * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (x = 0; s1[x] != '\0'; x++)
+		s[x] = s1[x];
+	for (h = 0; h <= n ; h++)
+		s[L1 + h] = s2[h];
+	s[L1 + n] = '\0';
 	return (s);
 }
