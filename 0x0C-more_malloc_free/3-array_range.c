79c11c5461946400ac6dccc9e7c76333ecc44d66
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-07 12:38:33 +0100

    fix betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,17 +14,16 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	
-	if ((max - min +1) < 0)
+	if ((max - min + 1) < 0)
 		memo = (-1) * (max - min + 1);
-	if ((max - min +1) >= 0)
+	if ((max - min + 1) >= 0)
 		memo = (max - min + 1);
 	arr = malloc(memo * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i <= memo; i++)
+	for (i = 0; i < memo; i++)
 	{
-		arr[i] = min +i;
+		arr[i] = min + i;
 	}
 	return (arr);
 }
