6b5ce858ef3b9768cc77e4acd7f8bf269ab37dc0
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-08 17:38:32 +0100

    check if the struct not empty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,5 +6,8 @@
 */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d != NULL) /*you check whether an object that should be pointed to by the pointer  exist!*/
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
 }
