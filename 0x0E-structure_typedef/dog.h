6ded8851974247589689e6e901ba9d4cb63b7532
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-08 15:59:24 +0100

    make a struct in a .h file

---
+++ 0x0E-structure_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+/**
+* struct dog - a struct for a dog
+* @name: its name
+* @age:its age
+* @owner: his owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
