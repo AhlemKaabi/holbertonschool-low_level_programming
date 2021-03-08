4f8e9f576f6787f5bafcf8f5f5cd5d1cce506f64
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-08 18:51:48 +0100

    function with a typedef type

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+* new_dog - function that creates new dog
+* @name: is the new name
+* @age: is the new age
+* @owner: is the new owner
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *N = malloc(sizeof(dog_t));
+	if (N != NULL)
+	{
+		N->name = name;
+		N->age = age;
+		N->owner = owner;
+		return (N);
+	}
+	else
+		return (NULL);
+}
