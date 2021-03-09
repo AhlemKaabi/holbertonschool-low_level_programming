5392f847754caf9dd34c341018946e62e0ee9d3b
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-09 12:07:56 +0100

    fixing code , requirment default

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 /**
 * new_dog - function that creates new dog
 * @name: is the new name
@@ -10,15 +11,36 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *N = malloc(sizeof(dog_t));
+	int i;
+	char *cpy_name, *cpy_owner;
+	dog_t *N;
 
-	if (N != NULL)
-	{
-		N->name = name;
-		N->age = age;
-		N->owner = owner;
-		return (N);
-	}
-	else
+	cpy_name = malloc(sizeof(name + 1));
+	if (cpy_name == NULL)
 		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		cpy_name[i] = name[i];
+	}
+	cpy_name = '\0';
+	cpy_owner = malloc(sizeof(owner + 1));
+	if (cpy_owner == NULL)
+		return (NULL);
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		cpy_owner[i] = owner[i];
+	}
+	cpy_owner = '\0';
+	N = malloc(sizeof(dog_t));
+	if (N == NULL)
+	{
+		free(cpy_name);
+		free(cpy_owner);
+		free(N);
+		return (NULL);
+	}
+	N->name = cpy_name;
+	N->age = age;
+	N->owner = cpy_owner;
+	return (N);
 }
