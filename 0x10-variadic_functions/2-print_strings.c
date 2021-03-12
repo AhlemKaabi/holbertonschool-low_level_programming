a1405a44bf556c4b078487b098a05821620264c6
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-12 09:37:57 +0100

    last test

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,10 +13,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ptr; /*pointer to the first argument*/
 
+	va_start(ptr, n);
 	s = va_arg(ptr, char*);
 	if (n != 0)
 	{
-		va_start(ptr, n);
 		if (separator == NULL)
 		{
 			for (i = 0; i < n; i++)
@@ -24,49 +24,42 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				if (s == NULL)
 				{
 					printf("(nil)");
-					printf("test1");
 				}
 					
 				else
 				{
 					printf("%s", s);
-					printf("test2");
 				}
 					
 			}
 		}
 		else
 		{
-			s = va_arg(ptr, char*);
 			for (i = 0; i < n - 1; i++)
 			{
 				if (s == NULL)
 				{
 					printf("(nil)%s", separator);
-					printf("test3");
 				}
 					
 				else
 				{
 					printf("%s%s", s, separator);
-					printf("test4");
 					/* 'char*' is the expected arg type ! */
 				}
 			}
+			s = va_arg(ptr, char*);
 			if (s == NULL)
 			{
 				printf("(nil)");
-				printf("test5");
 			}
 			else
 			{
 				printf("%s", s);
-				printf("test6");
 
 			}
 		}
 	}
 	printf("\n");
-	printf("test7");
 	va_end(ptr);
 }
