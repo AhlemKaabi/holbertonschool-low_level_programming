f9695e4f3c647b0c0e1ae641fa5f664d66b0ef9e
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-19 15:37:38 +0100

    first use of linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+* print_list - function that prints all the elements of a list_t list
+* @h: list_t pointer to a list node
+* Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
