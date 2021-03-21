acf82c755da8a49ffa2ae6d42909d8ae5fe6fc1c
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-21 23:39:11 +0100

    print a struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,13 +6,20 @@
 */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	/**
-	* you check whether an object
-	* that should be pointed to by the
-	* pointer  exist!
-	*/
+	if (d == NULL)
 	{
+		return;
+	}
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+		}
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 	else
