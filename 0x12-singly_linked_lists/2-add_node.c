7bdd2540b25c7fb3b595659ee7083eabd4343091
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-21 14:37:15 +0100

    add a new node at the beginningof a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,29 +4,32 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-* add_node - function that adds a new node at the beginning of a list_t list.
-* @h: list_t pointer to a list node
-* Return: number of nodes
+* add_node - function that adds a new node at the beginning
+* @head: list_t pointer to a structure node
+* @str: constant pointer to a string
+* Return: the new added node
 */
-size_t print_list(const list_t *h)
+list_t *add_node(list_t **head, const char *str)
 {
-	int i;
+	char *s;
+	int i, len = 0;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	if (h == NULL)
-		return (0);
-	i = 0;
-	while (h != NULL)
+	/* -- duplicate the str in another string of type char * -- */
+	s = strdup(str);
+	/* -- calculate the length of the string -- */
+	for (i = 0; *(str + i) != '\0'; i++)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%u] %s\n", h->len, h->str);
-		}
-		i++;
-		h = h->next;
+		len = i + 1;
 	}
-	return (i);
+	/*-- create the new node --*/
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = s;
+	new_node->len = len;
+	new_node->next = *head; /*dereference to the rest of the list*/
+	*head = new_node; /*change the head pointer to points to the new node*/
+	return (new_node);
 }
