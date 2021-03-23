ff0fa78254394fe038ddc2e4f6bedd5a70705b04
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-23 11:36:13 +0100

    delete first node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,21 +5,29 @@
 /**
 * pop_listint - function that deletes the head node of a listint_t linked list,
 * and returns the head node’s data (n).
-* @head: listint_t pointer to a list node
+* @head: listint_t pointer to a list node.
+* Return: the head node’s data (n).
 */
 int pop_listint(listint_t **head)
 {
-	int elem = 0;
-	listint_t *curr = *head;
+	listint_t *temp;
+	/*will replace the hread pointer that pointes to the first node */
 
-	
-	if (*head == NULL)
+	if (*head == NULL)/*check empty list -- our fist node is not empty*/
 		return (0);
-	curr->next->n = elem;
-//	curr = curr->next;
-//	*head = (*head)->next;/*change pointer (head) to the next node*/
-	
+	temp = *head; /* take the same adress as head*/
+	/*
+	* the pointer temp take what [*head]
+	* points to which is the desired first node to remove
+	*/
+	*head = (*head)->next;
+	/*
+	*change pointer (head) to the next node --
+	*thus change the adress of the head pointer to the next node
+	*/
+	if (temp == NULL)
+		return (0);
+	else
+		return ((temp)->n);
 	return (0);
 }
-//(*head)->n = elem;
-	//elem = (*head)->n;
