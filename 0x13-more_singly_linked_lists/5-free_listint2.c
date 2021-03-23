c46b3a531ea65beac867a8f2c8d8d2c70c680fe0
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-23 01:03:47 +0100

    modify line 13

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 	/*listint_t *curr = *head;*/
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		aux = *head;
 		*head = (*head)->next;/*-- curr = (*head) --*/
 		free(aux);
 	}
-	(head) = NULL;
+	head = NULL;
 }
