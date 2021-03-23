28d3295eb90027de48b1e6c6a3426886dd9a7cde
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-23 12:12:52 +0100

    get a node from a index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,14 +12,12 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *temp;
 
 	if (head == NULL)
 		return (NULL);
-	temp = head;
 	for (i = 0; i < index; i++)
 	{
-		temp = temp->next;
+		head = head->next;
 	}
-	return (temp);
+	return (head);
 }
