e7aa8e23dfa7cd4b75629fdf169a45585330cbdb
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-24 11:19:05 +0100

    insert node at index 0.4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	if (*head == NULL || idx == 0)
+	if (*head == NULL || idx == 0)/*inserst at the beginning with empty list or not */
 	{
 		new_node->n = n;
 		new_node->next = *head;
