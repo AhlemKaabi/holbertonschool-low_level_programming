a1b6c9d2da7dc7c673fbd77ebf54b069f41b6b16
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-25 21:58:46 +0100

    adding .hfile

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,4 +2,6 @@
 #define LISTS_H
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
