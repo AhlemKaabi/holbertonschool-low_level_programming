fbbd0711682f4f69e1c8e059fc798b443c2ad3db
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-26 13:48:58 +0100

    flipped number

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int count, flipped;
+	unsigned int count = 0, flipped;
 
 	if (n == 0 && m == 0)
 	{
@@ -17,12 +17,37 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	}
 	else
 	{
+		/*get the bit number that contain the flipped bites*/
+		/*using the XOR operand*/
 		flipped = n ^ m;
-		count = 0;
+		if (flipped == 0)
+			return (count);
+		/**
+		* while (flipped > 0)
+		* {
+		*	count++;
+		*	flipped = flipped & (flipped - 1);
+		*
+		*	[ flipped - 1 ] : delete the first bit seted (the first 1 bit)
+		*	[ flipped & (flipped - 1) ] this comparation will return the flipped number without that first bit (= 1)
+		*	and so on until the all bits that constitute the filpped bit number == 0
+		* }
+		*/
+		/*count set bits of the result = flipped bit number. e.g : flipped = 0101001 --> 3*/
 		while (flipped > 0)
 		{
-			count++;
-			flipped = flipped & (flipped - 1);
+      		count += flipped & 1;
+     		flipped >>= 1;
+		/**
+		* this methos is more simple it start to comapre the LSB of the flipped bit number
+		* with 1 and shift right the whole number
+		* e.g : flipped = 10110110
+		* LSB = 0
+		* now compare with 1 if you found 1 add 1 to count if not add 0.
+		* flipped >> 1 = 01011011 : our new flipped number
+		* now LSB = 1
+		* until the flipped number becomes 000000000000...... according to the bit number size(8 bits / 32bits ...)
+		*/
 		}
 		return (count);
 	}
