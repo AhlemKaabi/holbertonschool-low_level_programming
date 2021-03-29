0f5db8caead7acb2825543279a7da8d19f07df75
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-29 19:08:46 +0100

    print binary task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,11 +2,12 @@
 #include "holberton.h"
 /**
 * print_binary - print binary.
-*@n: the input number to print in binary 
+*@n: the input number to print in binary
 */
 void print_binary(unsigned long int n)
 {
-	unsigned long int length = 0, i, x, bit;
+	/* 1 << 39 == 0 WITHOUT 1L*/
+	unsigned long int length = 0, x, i, bit;
 
 	if (n == 0)
 		_putchar('0');
@@ -19,12 +20,11 @@ void print_binary(unsigned long int n)
 	i = 0;
 	while ((length - i) > 0)
 	{
-		bit = n & (1 << (length - 1 - i));
+		bit = n & (1L << (length - 1 - i)); /* 1L is 1 type long*/
 		if (bit == 0)
 			_putchar('0');
 		else
 			_putchar('0' + 1);
 		i++;
 	}
-}	
-
+}
