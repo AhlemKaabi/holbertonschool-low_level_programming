103af34348f35aae8b5790aa5d4f9042ee0b9713
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-31 18:12:34 +0100

    modify code line 25

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
+	if (text_content != NULL) /*to avoid segmentation fault*/
 	{
 		i = 0;
 		while (text_content[i] != '\0')
