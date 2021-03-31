919f26c8796e3bb93bf1cf5fab8bab96dd207ae0
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-31 18:13:23 +0100

    modify line 27

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,10 +24,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	i = 0;
-	while (text_content[i] != '\0')
+	if (text_content != NULL) /*to avoid segmentation fault*/
 	{
-		length++;
-		i++;
+		i = 0;
+		while (text_content[i] != '\0')
+		{
+			length++;
+			i++;
+		}
 	}
 	Cre = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (Cre == -1)
