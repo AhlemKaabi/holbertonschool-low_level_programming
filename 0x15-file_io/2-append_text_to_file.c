c6d85408b5bb5dc73abff424cf6bae7446f64dfb
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-31 17:35:06 +0100

    append text to a file task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -37,7 +37,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 		return (-1);
 	}
-	APP = open(filename, O_RDWR | O_APPEND, 0600);
+	APP = open(filename, O_WRONLY | O_APPEND);
 	if (APP == -1)
 	{
 		return (-1);
