1bcee2dc07e0d5aa89f7efe647d315c49a49209c
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-31 16:33:01 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,18 +30,15 @@ int create_file(const char *filename, char *text_content)
 		length++;
 		i++;
 	}
-	if (text_content != NULL) /*better than check == NULL FIRST!*/
+	Cre = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_RDONLY, 0600);
+	if (Cre == -1)
 	{
-		Cre = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_RDONLY, 0600);
-		if (Cre == -1)
-		{
-			return (-1);
-		}
-		printed = write(Cre, text_content, length);
-		if (printed == -1L)
-		{
-			return (0);
-		}
+		return (-1);
+	}
+	printed = write(Cre, text_content, length);
+	if (printed == -1L)
+	{
+		return (0);
 	}
 	close(Cre);
 	return (1);
