32bdb210f1fa093649b9abe7984c0bc3356243c3
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-31 13:52:59 +0100

    if read fail return 0 else print

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -42,15 +42,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		return (0);
 	}
-	/* syntax : ssize_t read(int fd, void *buf, size_t count); */
-	printed = write(STDOUT_FILENO, buf, read_buf);
-	if (printed == -1UL)
+	else
 	{
-		return (0);
+		/* syntax : ssize_t read(int fd, void *buf, size_t count); */
+		printed = write(STDOUT_FILENO, buf, read_buf);
+		if (printed == -1UL)
+		{
+			return (0);
+		}
+		/* write - write to a file descriptor */
+		/* ssize_t write(int fd, const void *buf, size_t count); */
+		/*STDOUT_FILENO : is the file descriptor (1)*/
 	}
-	/* write - write to a file descriptor */
-	/* ssize_t write(int fd, const void *buf, size_t count); */
-	/*STDOUT_FILENO : is the file descriptor (1)*/
 	free(buf);
 	close(fd);
 	return (printed);
