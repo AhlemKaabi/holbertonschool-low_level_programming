2d189988d5f98ee52dc3588268b90ddc3d7b0b38
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-31 15:48:29 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* -- open & creat libraries -- */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/* -- read & write -- */
+#include <unistd.h>
+
+#include "holberton.h"
+
+/**
+ * create_file - function that creates a file.
+ *@filename: is the filename to create.
+ *@text_content: is the content of the file.
+ * Return: 1 on success.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int Cre, i, length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (text_content[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	if (text_content == NULL)
+	{
+		Cre = open(filename, O_CREAT | O_WRONLY | O_RDONLY, 0600);
+		if (Cre == -1)
+		{
+			return (-1);
+		}
+		write(Cre, text_content, length);
+		close(Cre);
+		return (1);
+	}
+	Cre = open(filename, O_CREAT | O_RDONLY | O_WRONLY, 0600);
+	if (Cre == -1)
+	{
+		return (-1);
+	}
+	write(Cre, text_content, length);
+	close(Cre);
+	return (1);
+}
