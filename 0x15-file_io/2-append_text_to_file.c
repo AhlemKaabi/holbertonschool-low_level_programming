c68500ebf5f0a16f51770c4503009a6ba81af654
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-03-31 17:16:51 +0100

    append text to a file task

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* -- open & creat libraries -- */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/* -- read & write -- */
+#include <unistd.h>
+
+#include "holberton.h"
+
+/**
+ * append_text_to_file - function that appends text at the end of a file.
+ *@filename: is the filename to create.
+ *@text_content: is the content to add at the end of the file.
+ * Return: 1 on success.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int APP, i, length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (text_content[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+	if (text_content == NULL)
+	{
+		if (filename != NULL)
+		{
+			return (1);
+		}
+		return (-1);
+	}
+	APP = open(filename, O_RDWR | O_APPEND, 0600);
+	if (APP == -1)
+	{
+		return (-1);
+	}
+	write(APP, text_content, length);
+	close(APP);
+	return (1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,4 +2,5 @@
 #define HOLBERTON_H
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 #endif
