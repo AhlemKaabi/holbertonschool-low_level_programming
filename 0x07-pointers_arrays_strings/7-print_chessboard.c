e1a71ad48e8e253e128ebfb942a73982e3062262
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-04-11 23:41:47 +0100

    adding more string functions

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,13 +9,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-int i, j;
-for (i = 0; i < 8; i++)
-{
-	for (j = 0; j < 8; j++)
+	int i, j;
+	for (i = 0; i < 8; i++)
 	{
-		printf("%c", a[i][j]);
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
 	}
-	printf("\n");
-}
 }
