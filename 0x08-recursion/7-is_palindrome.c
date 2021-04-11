9c007d09adea94ee16f65de9a2b6cede385f9cc8
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-04-12 00:08:13 +0100

    fix betty

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,46 +1,57 @@
 #include "holberton.h"
 /**
-*
+* check - recursive string comparison
+* @str: string
+* @index: starting occurence
+* @len: string length
+* Return: int
 */
-int check(char *s, int i)
+int check(char *str, int index, int len)
 {
-	if (*s != *(s + i))
+	if (*(str + index) != *(str + len))
 	{
 		return (0);
 	}
-	if (i > i /2)
+	if (index > len)
 	{
-		if (*s == *(s + i))
-			return (check(s++, i - 1));
-		else
-			return(0);
-	}
-	if (i == i/2 )
 		return (1);
+	}
+	else
+	{
+	/* len and index are on the same char */
+	return (check(str, index + 1, len - 1));
+	}
 }
+/**
+* _strlen_recursion - calculate length of string.
+* @s: the string
+* Return: int.
+*/
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-	
-	if (*s)
-	{
-		i++;
-		i += _strlen_recursion(s + 1);
-	}
-	
-	return(i);
-}
-
-int is_palindrome(char *s)
-{
-	int len;
 	if (*s == '\0')
 	{
-		return(1);
+		return (0);
 	}
-	if (*s != '\0')
+	return (_strlen_recursion(s + 1) + 1);
+}
+/**
+* is_palindrome - entry point
+* @s: string to check
+* Return: int
+*/
+int is_palindrome(char *s)
+{
+	int n;
+	/* an empty string is a palindrome */
+	if (*s == '\0')
 	{
-		len = _strlen_recursion(s);
+		return (1);
 	}
-	return(check(s , len));
+	/* get the length of the string s with recursive function */
+	else
+	{
+	n = _strlen_recursion(s);
+	}
+	return (check(s, 0, n - 1));
 }
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -6,4 +6,8 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int check(char *str, int index, int len);
+int _strlen_recursion(char *s);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2)
 #endif
