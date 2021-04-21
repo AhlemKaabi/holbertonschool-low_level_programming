c6125f7f83cedb1fb0fbe685568d21c694b75948
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-21 15:55:38 +0100

    Print the size-of various type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,15 @@
 
 /**
 * main - entry point
-* prints the alphabets in lowercase and uppercase
 * Return: success
 */
 
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
-printf("Size of a float: %lu byte(s)\n", sizeof(float));
-return (0);
+  printf("Size of a char: %lu byte(s)\n", sizeof(char));
+  printf("Size of an int: %lu byte(s)\n", sizeof(int));
+  printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+  printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+  printf("Size of a float: %lu byte(s)\n", sizeof(float));
+  return (0);
 }
