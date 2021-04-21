a28898f32862ad8db3991ec650432270fd2eaf29
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-04-21 17:27:35 +0100

    print numbers of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,17 +4,18 @@
 
 /**
 * main - entry point
-* prints the alphabets in lowercase and uppercase
+* print all the numbers of base 16 in lowercase.
 * Return: success
 */
 
 int main(void)
 {
-char ch0, ch1;
-for (ch0 = '0'; ch0 <= '9'; ch0++)
-putchar(ch0);
-for (ch1 = 'a'; ch1 <= 'f'; ch1++)
-putchar(ch1);
-putchar('\n');
-return (0);
+	char ch0, ch1;
+
+	for (ch0 = '0'; ch0 <= '9'; ch0++)
+		putchar(ch0);
+	for (ch1 = 'a'; ch1 <= 'f'; ch1++)
+		putchar(ch1);
+	putchar('\n');
+	return (0);
 }
