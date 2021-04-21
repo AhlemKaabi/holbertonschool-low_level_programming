deed0d4ab0e84130993769de4eafc210d027f4cd
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-04-21 16:50:45 +0100

    print all single digit numbers using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,9 +10,10 @@
 
 int main(void)
 {
-int i;
-for (i = 48 ; i < 58 ; i++)
-putchar(i);
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
