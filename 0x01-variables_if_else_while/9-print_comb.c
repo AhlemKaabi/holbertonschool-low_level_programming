e141873f001f578cfff7407871e64e53f8f79254
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-04-21 17:30:48 +0100

    combination of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,22 +4,23 @@
 
 /**
 * main - entry point
-* prints 10 decimal numbers
+* all possible combinations of single-digit numbers.
 * Return: success
 */
 
 int main(void)
 {
-int i, j;
-for (i = 48 ; i < 58 ; i++)
-{
-putchar(i);
-for (j = 0 ; ((j < 1) && (i < 57)); j++)
-{
-putchar(44);
-putchar(32);
-}
-}
-putchar('\n');
-return (0);
+	int i, j;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		putchar(i);
+		for (j = 0 ; ((j < 1) && (i < 57)); j++)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
