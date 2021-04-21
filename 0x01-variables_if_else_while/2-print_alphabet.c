62b649905fc39b20f0053a5f3e7c2552cf95dbbf
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-04-21 16:40:28 +0100

    print alphabet using putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,9 +10,9 @@
 
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-putchar('\n');
-return (0);
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
 }
