f6d654d6183c380904fb12b32ef10d26096dbddd
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 00:50:48 +0100

    Function that prints 10 times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
 * print_alphabet_x10 - 10 lines alphabet
-* Return: success
 */
 void print_alphabet_x10(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
-{
-for (j = 'a'; j <= 'z'; j++)
-{
-_putchar(j);
-}
-_putchar('\n');
-}
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
+	}
 }
