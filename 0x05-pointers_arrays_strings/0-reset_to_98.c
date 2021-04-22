910c710fa00a1d350c0ad0c40d9b6b31aab1891a
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 02:54:58 +0100

    reset an integer using a pointer variable

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
 * reset_to_98 - function that takes a pointer to an int as
 * parameter and updates the vaalue it points to to 98.
-*@n: parameter
+* @n: parameter
 */
 void reset_to_98(int *n)
 {
