3d2275daa1d869c4cf06384e569163400d35013a
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 02:33:56 +0100

    draw a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,12 +14,12 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j <= i - 1; j++)
 			{
-			_putchar(32);
+				_putchar(32);
 			}
 			_putchar(92);
 			_putchar('\n');
 		}
 	}
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
