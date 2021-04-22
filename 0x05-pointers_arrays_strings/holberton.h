6f5bf442bca03a08bfaac2349612213eba77b5ed
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 03:14:08 +0100

    Update holberton.h

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,4 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
 #endif
