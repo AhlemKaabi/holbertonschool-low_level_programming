9c48cb5cc84c6f26935625b7e802312f0e908687
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 03:17:52 +0100

    print numbers

---
+++ 0x06-pointers_arrays_strings/100-print_numbers.c
@@ -0,0 +1,38 @@
+
+#include "holberton.h"
+/**
+ *print_number - function that prints integer.
+ *@n: number to print.
+ */
+void print_number(int n)
+{
+	unsigned int i, digit_count, aux, factor, numb;
+
+	digit_count = factor = 1;
+	numb = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		numb = numb * (-1);
+	}
+	aux = numb;
+	/* while the number is composed of more than 1 digit */
+	while (aux > 9)
+	{
+		aux = aux / 10;
+		digit_count++;
+	}
+
+	for (i = 1; i < digit_count; i++)
+	{
+		factor = factor * 10;
+	}
+
+	while (factor > 1)
+	{
+		_putchar(((numb / factor) % 10) + '0');
+		/*print the first digit, from the left*/
+		factor = factor / 10;
+	}
+	_putchar((numb % 10) + '0'); /*print last digit*/
+}
