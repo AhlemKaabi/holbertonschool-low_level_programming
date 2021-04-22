2a851fdbc90df0889c95b8a1a35936370bab515b
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 23:10:09 +0100

    Custom strcat function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,24 @@
 # include "holberton.h"
 /**
-* _strcat - function
-*@dest: para
-*@src: para
-* Return: string
+* _strcat - function that concatenates two strings.
+* @dest: the destination string
+* @src: the source string
+* Return: the destination string modified
 */
 char *_strcat(char *dest, char *src)
 {
-char *p = dest;
-while (*dest != '\0')
-{
-dest++;
-}
-while (*src != '\0')
-{
-*dest = *src;
-dest++;
-src++;
-}
-*dest = '\0';
-return (p);
+	char *p = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (p);
 }
