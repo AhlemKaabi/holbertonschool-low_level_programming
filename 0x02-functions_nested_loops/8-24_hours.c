8c3af9b853b72892e3f21f3ec98679e50c9a8f27
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 01:00:32 +0100

    print the time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
 /**
- * jack_bauer - check the code for Holberton School students.
+ * jack_bauer - function that prints every minute of the day
+ * of Jack Bauer, starting from 00:00 to 23:59.
  * Return: Always 0.
  */
 void jack_bauer(void)
-{	int i, j;
+{	
+	int i, j;
+
 	for (i = 0; i <= 23; i++)
 	{
 		for (j = 0; j <= 59; j++)
