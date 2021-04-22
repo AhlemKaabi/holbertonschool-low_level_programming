a8187cff9738b99bd54ef767ca4b10bf1325ba1e
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 02:30:20 +0100

    print numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* print_most_numbers - funtion that print numbers from 0 to 9
+* print_most_numbers - funtion that print numbers from 0 to 9.
 * Return: success or fail
 */
 int print_most_numbers(void)
