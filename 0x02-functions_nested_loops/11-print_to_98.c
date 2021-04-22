da0f79bf2e8a71d5b3cd0d9cbd2959d169b3d82f
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 01:06:00 +0100

    print numbres

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-* print_to_98 - prints last digit
+* print_to_98 - function that prints all natural numbers
+* from n to 98, followed by a new line.
 * @n: parameter
-* Return: success
 */
 void print_to_98(int n)
 {
