5b21d98d23e5e3976ea05e8661d738e002e409c4
AhlemKaabi <71605778+AhlemKaabi@users.noreply.github.com>
2021-04-22 23:14:42 +0100

    Custom strncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,29 @@
 # include "holberton.h"
 /**
-* _strncat - function
-*@n: para
-*@dest: para
-*@src: para
-* Return: string
+* _strncat - function that concatenates two strings
+* it will use at most n bytes from src.
+* @n: number of bytes to copy
+* @dest: the destination string
+* @src: the source string
+* Return: a pointer to the resulting string dest
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-char *p = dest;
+	int i;
+	char *p = dest;
 
-while (*dest != '\0')
-{
-dest++;
-}
-i = 0;
-while (*src != '\0' && i <= n - 1)
-{
-*dest = *src;
-dest++;
-src++;
-i++;
-}
-*dest = '\0';
-return (p);
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	i = 0;
+	while (*src != '\0' && i <= n - 1)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	*dest = '\0';
+	return (p);
 }
