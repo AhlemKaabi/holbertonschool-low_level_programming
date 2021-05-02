3be3bd69d4fbaa626bcb87405741f549d0137d80
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-05-02 06:53:59 +0100

    add a node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that returns the nth node
+ * of a dlistint_t linked list.
+ *@head: pointer to the first node of the list.
+ *@index: the index of the node.
+ * Return: pointer to the new node.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *node;
+
+	node = head;
+	i = 0;
+	while (node->next != NULL)
+	{
+		if (i == index)
+		{
+			return (node);
+		}
+		if (i < index)
+		{
+			node = node->next;
+			i++;
+		}
+	}
+	return (NULL);
+}
