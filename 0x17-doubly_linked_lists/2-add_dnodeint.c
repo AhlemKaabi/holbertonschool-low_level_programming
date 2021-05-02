2505a678036d59a58efb3b9acc6fe713e9d86b0a
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-05-02 06:24:31 +0100

    add a new  node to the D linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -28,7 +28,8 @@ dlistint_t *create_new_node(int n)
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	if(head == NULL)
+
+	if (head == NULL)
 	{
 		return (NULL);
 	}
