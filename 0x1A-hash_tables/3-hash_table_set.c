ee2582f421adc09a6f30deafa70e8825e5e15d84
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-05-28 16:03:49 +0100

    add an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	const char *value_cp;
-	hash_table_t *aux;
+	hash_node_t *aux;
 	hash_node_t *node;
 
 	node = malloc(sizeof(hash_node_t));
