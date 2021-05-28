88bca0fb8d2a9fd4764ed0ba7070c3e7421ab24b
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-05-28 15:51:46 +0100

    add an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,17 +24,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	printf("index is :%ld\n", index);
 	value_cp = strdup(value);
+	node->key = (char *)key;
+	node->value = (char *)value_cp;
 	if (ht->array[index] == NULL)
 	{
-		node->key = (char *)key;
-		node->value = (char *)value_cp;
+
 		node->next = NULL;
 		ht->array[index] = node;
 	}
 	else
 	{
-		node->key = (char *)key;
-		node->value = (char *)value_cp;
 		node->next = ht->array[index];
 		ht->array[index] = node;
 	}
