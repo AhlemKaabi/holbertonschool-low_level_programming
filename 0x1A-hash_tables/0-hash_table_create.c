ffa9314abb5c821cbbdbcfbd6ed960f0e909c21e
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-05-28 12:24:17 +0100

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,6 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL);
 	}
+	my_hash_table->size = size;
 	my_hash_table->array = malloc(sizeof(hash_node_t) * size);
 	for (i = 0; i < size; i++)
 	{
