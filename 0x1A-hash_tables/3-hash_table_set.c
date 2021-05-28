4af34e038177ad17fadf899a96538f585082e9c0
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-05-28 18:23:33 +0100

    duplicate the key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -37,6 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		aux = ht->array[index];
 		node->next = aux;
 		ht->array[index] = node;
+		node->next->next = NULL;
 	}
 	return (1);
 }
