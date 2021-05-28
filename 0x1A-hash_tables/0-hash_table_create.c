98959a01c858ceef129d0f721beeebeec5229c7c
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-05-28 12:01:16 +0100

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
+ * hash_table_create - function that creates a hash table.
+ * @size: The size of the array.
+ * Return: A pointer to the newly created hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -10,12 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 	my_hash_table = malloc(sizeof(unsigned long int) * size);
 	if (my_hash_table == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-	return my_hash_table;
+	return (my_hash_table);
 }
