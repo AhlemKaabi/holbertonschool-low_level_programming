01000a9624f0744ad9b8d3824318138c4e1c5aa5
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-06-03 11:03:03 +0100

    add separator variable

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,22 +7,26 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current;
 	unsigned long int i;
+	int separator;
 
 	if (ht == NULL)
 	{
 		return;
 	}
 	printf("{");
-	current = ht->array[0];
 	i = 0;
+	separator = 0;
 	while (i < ht->size)
 	{
 		current = ht->array[i];
-		while (current != NULL)
+		while (current)
 		{
+			if (separator == 1)
+			{
+				printf(", ");
+			}
 			printf("\'%s\' : \'%s\'", current->key, current->value);
-			/*add condition!*/
-			printf(", ");
+			separator = 1;
 			current = current->next;
 		}
 		i++;
